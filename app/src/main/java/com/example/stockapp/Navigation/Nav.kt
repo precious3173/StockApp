fc398e047e8c8965554c87afcb1f2c5c8cdcf0b3
Admin <preciouspeter3173@gmail.com>
2022-12-15 15:22:53 +0100

    Bottom Navigation added

--- app/src/main/java/com/example/stockapp/Navigation/Nav.kt
+++ app/src/main/java/com/example/stockapp/Navigation/Nav.kt
@@ -1,12 +1,22 @@
 package com.example.stockapp.Navigation
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -14,25 +24,110 @@ import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.stockapp.R
+import com.example.stockapp.SplashScreens
 
 @Composable
-fun Nav(navController: NavHostController) {
+fun Nav() {
 
+    val navController = rememberNavController()
+    var bottomState by remember {
+        mutableStateOf("Stocks")
+    }
     NavHost(navController = navController,
         startDestination = "splash_screen"
     ){
 
         composable("splash_screen"){
+            SplashScreens(navController = navController)
 
         }
-        composable("stock_screen"){
-            Column (
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
 
-            ){
-                Text(text = "Stock Screen", fontSize = 30.sp, fontWeight = FontWeight.Bold )
+        composable("Stocks"){
+
+          //  com.example.stockapp.ui.theme.BottomNavigation()
+
+
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+
+
+            )
+
+
+            {
+
+            }
+
+            Scaffold {
+
+                Scaffold (
+
+                    content = {
+                       Box(
+                         modifier = Modifier.fillMaxSize(),
+                           Alignment.Center
+                       ){
+                           Text(text = bottomState,
+                             fontSize = 25.sp,
+                               fontWeight = FontWeight.Bold
+                               )
+                       }
+
+                    },
+                    bottomBar  =  {
+
+                        androidx.compose.material.BottomNavigation(
+                           backgroundColor = colorResource(id = R.color.pink)
+
+                        ) {
+                            BottomNavigationItem(selected = bottomState == "Stocks" ,
+                                onClick = { bottomState = "Stocks"},
+                                label = { Text(text = "Stocks")},
+                                icon = {Icon( painterResource( R.drawable.ic_baseline_inventory_24),
+                                    contentDescription = null)
+                                       },
+
+                                )
+
+                            BottomNavigationItem(selected = bottomState == "Scan" ,
+                                onClick = { bottomState = "Scan"},
+                                label = { Text(text = "Scan")},
+                                icon = { Icon(painterResource( id = R.drawable.ic_baseline_qr_code_scanner_24),
+                                    contentDescription = null)
+                                },
+
+                                )
+                            BottomNavigationItem(selected = bottomState == "Report" ,
+                                onClick = { bottomState = "Report"},
+                                label = { Text(text = "Report")},
+                                icon = { Icon(painterResource( id = R.drawable.ic_baseline_bar_chart_24),
+                                    contentDescription = null)
+                                },
+
+                                )
+
+                            BottomNavigationItem(selected = bottomState == "Settings" ,
+                                onClick = { bottomState = "Settings"},
+                                label = { Text(text = "Settings")},
+                                icon = { Icon(painterResource( R.drawable.ic_baseline_settings_24),
+                                    contentDescription = null)
+                                },
+
+                                )
+
+                        }
+
+                    }
+                )
+
+
             }
 
         }
--- app/src/main/java/com/example/stockapp/SplashScreen.kt
+++ app/src/main/java/com/example/stockapp/SplashScreen.kt
@@ -3,73 +3,102 @@ package com.example.stockapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph
-import androidx.navigation.Navigation
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.stockapp.Navigation.Nav
-import com.example.stockapp.ui.theme.StockAppTheme
 import kotlinx.coroutines.delay
+
 
 class SplashScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val navController = rememberNavController()
-            Nav(navController)
-           SplashScreens(navController)
+
+            Nav()
+
+
+            var animation by remember {
+
+                mutableStateOf(false)
+            }
+            var anime = animateFloatAsState(
+                targetValue = if(animation) 1f else 0f,
+                 animationSpec = tween(
+                     durationMillis = 5000
+                 )
+                )
+
             }
         }
     }
 
 
 @Composable
-fun SplashScreens(navController: NavController){
+fun SplashScreens(navController: NavHostController){
+
+
     LaunchedEffect(key1 = true){
 
-        delay(5000L)
+        delay(3000L)
 
-        navController.navigate("stock_screen")
+        navController.navigate("Stocks"){
+           // popUpTo("home")
+        }
     }
     Column(
-        modifier = Modifier.fillMaxSize().background(Color.Magenta),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta),
         verticalArrangement =  Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
 
     ) {
+
+        Text(text = "Inventory Manager", fontWeight = FontWeight.Bold,
+            fontSize = 35.sp,color = Color.White, fontFamily = FontFamily.Cursive,
+            modifier = Modifier.padding(20.dp)
+
+        )
+
         Image(painter = painterResource(id = R.drawable.inventory),
-            contentDescription = "Inventory Check",
-            modifier = Modifier.width(200.dp)
+            contentDescription = "",
+            modifier = Modifier
+                .width(200.dp)
                 .height(200.dp))
+        
+
     }
 }
 
 @Composable
-fun nav(){
-
-    val navController = rememberNavController()
-
-
-
+@Preview
+fun SplashScreenPreview(){
+    SplashScreen()
 }
 
 
