ba88557a62b9cec4b97438085b35a282058938fe
Admin <preciouspeter3173@gmail.com>
2023-01-06 23:18:03 +0100

    recyclerview item updated

--- app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
+++ app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
@@ -1,13 +1,16 @@
 package com.example.stockapp.Recyclerview
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,7 +18,7 @@ fun StockItem() {
     Card(modifier = Modifier
         .padding(8.dp, 4.dp)
         .fillMaxWidth()
-        .height(110.dp),
+        .height(110.dp).background(color = Color.Magenta),
     shape = RoundedCornerShape(8.dp),
         elevation = 4.dp
     ) {
@@ -27,9 +30,9 @@ fun StockItem() {
           verticalArrangement = Arrangement.Center,
           horizontalAlignment = Alignment.CenterHorizontally){
 
-              Text(text = "Stock name")
-              Text(text = "Stock Location")
-              Text(text = "Barcode Number")
+              Text(text = "Stock name", modifier = Modifier.background(color = Color.White).padding(10.dp), style = MaterialTheme.typography.caption)
+              Text(text = "Stock Location", modifier = Modifier.background(color = Color.White).padding(10.dp), style = MaterialTheme.typography.caption)
+              Text(text = "Barcode Number", modifier = Modifier.background(color = Color.White).padding(10.dp), style = MaterialTheme.typography.caption)
 
           }
 
