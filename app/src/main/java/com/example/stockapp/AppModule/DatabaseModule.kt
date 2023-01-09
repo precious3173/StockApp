1286dfc50e91a7e0adc5ef3a71998462fa5e8072
Admin <preciouspeter3173@gmail.com>
2023-01-09 23:36:55 +0100

    Room database Instance created and the databasebuilder completed

--- app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
@@ -1,11 +1,45 @@
 package com.example.stockapp.AppModule
 
+import android.app.Application
+import androidx.room.Room
+import com.example.stockapp.Database.StockDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
-@Singleton
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
 
+    @Volatile
+    private var INSTANCE: StockDatabase? = null
+
+
+     @Singleton
+     @Provides
+    fun getDatabase(app: Application) : StockDatabase{
+
+        val instance = INSTANCE
+
+        if (instance != null) return instance
+
+        synchronized(this){
+
+            val instanceofDatabase = Room.databaseBuilder(
+                app.applicationContext, StockDatabase::class.java,
+                "Stock_Database"
+            ).allowMainThreadQueries().build()
+
+            INSTANCE = instanceofDatabase
+
+            return instanceofDatabase;
+        }
+
+
+
+    }
 
 }
